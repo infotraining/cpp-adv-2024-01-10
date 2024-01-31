3b7dd3d8c246e8e80918c6029e90c4658cbff12b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-01-31 09:23:06 +0100

    Add intro-cpp

---
+++ intro-cpp/tests.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <memory>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using Utils::Gadget;
+
+TEST_CASE("test")
+{
+    // TODO
+}
