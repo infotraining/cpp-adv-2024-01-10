1ed60fa64b95764f0e7fd976cb02eb6def9bbe24
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-01-31 12:47:39 +0100

    Array - 2

--- intro-cpp/tests.cpp
+++ intro-cpp/tests.cpp
@@ -1,35 +1,65 @@
 #include "utils.hpp"
 
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <list>
 
 using Utils::Gadget;
 
 class Array
 {
-public:
+public:    
+    using iterator = int*; // typedef int* iterator;
+    using const_iterator = const int*; // typedef const Pint* iterator;
+
     Array(size_t size)
-        : size_{size}, items_{(int*)malloc(size_ * sizeof(int))}
+        : size_{size}
+        , items_{new int[size]}
     {
         // for(size_t i = 0; i < size_; ++i)
         //     items_[i] = 0; // *(items_ + i) = 0
-        
+
         std::fill_n(items_, size_, 0);
+    }
+
+    Array(std::initializer_list<int> il) : size_{il.size()}, items_{new int[size_]}
+    {
+        std::copy(il.begin(), il.end(), items_);
     }
 
     ~Array()
     {
-        free(items_);
+        delete[] items_;
     }
 
     size_t size() const
     {
         return size_;
-    }    
+    }
+
+    iterator begin()
+    {
+        return items_;
+    }
+
+    const_iterator begin() const
+    {
+        return items_;
+    }
+
+    iterator end()
+    {
+        return items_ + size_;
+    }
+
+    const_iterator end() const
+    {
+        return items_ + size_;
+    }
 
     const int& operator[](size_t index) const // read-only
     {
@@ -53,7 +83,7 @@ public:
     {
         if (index >= size_)
             throw std::out_of_range("Index out of bounds");
-            
+
         return items_[index];
     }
 
@@ -76,11 +106,12 @@ TEST_CASE("init variables")
 
 void print(const Array& arr)
 {
-    std::cout << "{ ";    
-    for (int i = 0; i < arr.size(); ++i)
+    std::cout << "{ ";
+    for(auto it = arr.begin(); it != arr.end(); ++it)
     {
-        std::cout << arr[i] << " ";
-    }
+        std::cout << *it << " ";
+    }        
+
     std::cout << "}\n";
 }
 
@@ -95,10 +126,107 @@ TEST_CASE("dynamic arrays")
         free(raw_mem);
     }
 
+    SECTION("C++")
+    {
+        SECTION("new - unit alloc")
+        {
+            int* ptr = new int(13); // allocation
+            *ptr = 42;
+            CHECK(*ptr == 42);
+            delete ptr; // de-allocation
+        }
+
+        SECTION("new - table alloc")
+        {
+            int* arr = new int[100];
+            arr[0] = 42;
+            delete arr;
+        }
+    }
+}
+
+TEST_CASE("containers & iterators")
+{
+    SECTION("native arrays & pointers")
+    {
+        const int size = 8;
+        int native_arr[size] = {};
+
+        int* start = native_arr;
+        int* post_end = native_arr + size;
+
+        for (auto it = start; it != post_end; ++it)
+        {
+            *it = 42;
+        }
+    }
+
+    SECTION("container")
+    {
+        std::list<int> vec = {1, 2, 3, 4, 5};        
+
+        for (auto it = vec.begin(); it != vec.end(); ++it)
+        {
+            *it = 42;
+        }
+    }
+
+    SECTION("range-based for")
+    {
+        SECTION("native array")
+        {
+            const int size = 8;
+            int native_arr[size] = {};
+
+            for(auto& item : native_arr)
+            {
+                item = 42;
+            }
+        }
+
+        SECTION("container")
+        {
+            std::list<int> vec = {1, 2, 3, 4, 5};        
+
+            for (const auto& item : vec)
+            {
+                std::cout << item << " ";
+            }
+            std::cout << "\n";
+
+            SECTION("is interpreted as")
+            {
+                auto start = vec.begin();
+                auto post_end = vec.end();
+
+                for (auto it = start; it != post_end; ++it)
+                {
+                    const auto& item = *it;
+                    std::cout << item << " ";
+                }
+            }
+        }
+
+        SECTION("Array")
+        {
+            Array arr = {1, 2, 3, 4, 5, 6};
+
+            for(auto& item : arr)
+            {
+                item = 2 * item;
+            }
+
+            for(const auto& item : arr)
+            {
+                std::cout << item << " ";
+            }
+            std::cout << "\n";
+        }
+    }
 }
 
 TEST_CASE("Array - basic interface")
-{    
+{
     Array data1{10};
 
     int value = data1[0];
@@ -111,10 +239,12 @@ TEST_CASE("Array - basic interface")
 
     CHECK(data1.size() == 10);
 
-    // for(int i = 0; i < data1.size(); ++i)
-    // {
-    //     data1[i] = i * i;
-    // }
+    for(int i = 0; i < data1.size(); ++i)
+    {
+        data1[i] = i * i;
+    }
 
-    // CHECK(data1[3] == 4);
+    CHECK(data1[3] == 4);
+
+    std::fill_n(data1.begin(), 4, 1024);
 }
